0cf6de15f9bfdf8bd838ed0cc7f0ab0d88057948
Juan <argudojuan7@gmail.com>
2021-04-04 23:09:24 +0200

    First approach done. Waiting for new content from Laracasts

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts.posts');
 });
+
+Route::get('posts/{post}', function ($slug) {
+
+    return view('posts.post', [
+        'post' => Post::find($slug)
+    ]);
+
+})->where('post', '[A-z_\-]+');
